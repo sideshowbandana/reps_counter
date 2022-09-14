c783b3c5edf9c5aff6993ac9c6251b226fa1e949
Kyle Barton <kyle.humberto@gmail.com>
2022-09-14 10:29:25 -0700

    Add app icons

---
+++ MyWorkouts WatchKit Extension/Views/AccelerometerView.swift
@@ -0,0 +1,21 @@
+//
+//  AccelerometerView.swift
+//  MyWorkouts WatchKit Extension
+//
+//  Created by Kyle on 9/13/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccelerometerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccelerometerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccelerometerView()
+    }
+}
